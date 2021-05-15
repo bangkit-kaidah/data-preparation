d4460307649af17171a521f6f17f301bcedcdde2
novelworm777 <mikhaya.jrh@gmail.com>
2021-05-15 20:18:34 +0700

    add script validation for file size

--- script.py
+++ script.py
@@ -7,12 +7,16 @@ from convert import save_convert_pdf_to_txt
 DIRECTORY = 'downloads/'
 
 if __name__ == "__main__":
-    file_csv = read_csv(sys.argv[1])
-    url_df = file_csv['url']
+    csv_df = read_csv(sys.argv[1])
+    file_url_df = csv_df['file_url']
+    file_size_df = csv_df['file_size']
 
-    if not os.path.exists(DIRECTORY) and url_df.count+1 > 0:
+    if not os.path.exists(DIRECTORY):
         os.mkdir(DIRECTORY)
 
-    for url in url_df:
-        file_location = download_file(DIRECTORY, url)
+    for index, (file_url, file_size) in enumerate(zip(file_url_df, file_size_df)):
+        if index == 20: break
+        if file_size == 0:
+            continue
+        file_location = download_file(DIRECTORY, file_url)
         save_convert_pdf_to_txt(file_location)
